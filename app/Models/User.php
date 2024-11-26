f238fe82ca6543c9035b210981f1a29a6e9f70d2
EldarBruveris <eldar.bruverys@innowise.com>
2024-11-26 17:27:15 +0300

    findALl and save functions

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,5 +3,25 @@
 namespace app\Models;
 
 class User{
-    
+    private int $id;
+    private string $name;
+    private string $email;
+    private string $gender;
+    private string $status;
+
+    public function __construct(int $id, string $name, string $email, string $gender, string $status)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->gender = $gender;
+        $this->status = $status;
+    }
+
+    public function __get(string $name): mixed {
+        if (array_key_exists($name, $this->data)) {
+            return $this->data[$name];
+        }
+        return null;
+    }
 }
--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -1,6 +1,9 @@
 <?php
 namespace app\Repository;
 
+use app\Models\User;
+use PDO;
+
 class UserRepository extends AbsrtractRepository{
     //TODO поиск одного пользователя по email/ID/гендер... 
     public function find(array $critery = []){
@@ -8,12 +11,21 @@ class UserRepository extends AbsrtractRepository{
     }
 
     //TODO выбрать всех из БД
-    public function findAll(){
+    public function findAll(): array {
+        $users = [];
+        $query = 'SEECT * FROM users';
+        $statement = $this->connection->query($query);
+        while($row = $statement->fetch(PDO::FETCH_ASSOC)){
+            $users[] = new User($row['id'], $row['name'], $row['email'], $row['gender'], $row['status']);
+        }
 
+        return $users;
     }
 
     //TODO сохранение одного пользователя
-    public function save(){
-        
+    public function save(User $user): bool{
+        $query = "INSERT INTO users (id, name, email, gender, status) VALUES(:id, :name, :email, :gender, :status)";
+        $statement = $this->connection->prepare($query);
+        return $statement->execute([':id' => $user->id, ':name' => $user->name, ':email' => $user->email, ':gender' => $user->gender, ':status' => $user->status]);
     }
 }
