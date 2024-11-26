eb7de3ad6af422d5f36e76984c60baf3f0cf2134
EldarBruveris <eldar.bruverys@innowise.com>
2024-11-26 17:32:32 +0300

    failed try

--- app/Controllers/UserAction.php
+++ app/Controllers/UserAction.php
@@ -4,10 +4,17 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use app\Models\User;
+use app\Repository\UserRepository;
+
 final class UserAction
 {
     public function __invoke()
     {
-        echo 'User Action';
+        $db = new UserRepository;
+        $users = $db->findAll();
+        foreach ($users as $user) {
+            echo "ID: {$user->id}, Name: {$user->name}, Email: {$user->email}, Gender: {$user->gender}, Status: {$user->status}\n";
+        }
     }
 }
