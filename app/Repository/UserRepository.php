4d41a3e3be282d90f52da9fdc5aa527cee2a24f7
EldarBruveris <eldar.bruverys@innowise.com>
2024-12-26 01:25:12 +0300

    save before refactoring

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -11,7 +11,7 @@ class UserRepository extends AbstractRepository{
         $query = "SELECT * FROM users WHERE {$critery} = {$value}";
         $statement = $this->connection->query($query);
         $row = $statement->fetch(PDO::FETCH_ASSOC);
-        $user = new User($row['id'], $row['email'], $row['full_name'], $row['gender'], $row['status']);
+        $user = new User($row['id'], $row['full_name'], $row['email'], $row['gender'], $row['status']);
         return $user;
     }
 
@@ -32,5 +32,11 @@ class UserRepository extends AbstractRepository{
         $query = "INSERT INTO users (email, full_name, gender, status) VALUES(:email, :full_name, :gender, :status)";
         $statement = $this->connection->prepare($query);
         return $statement->execute([':email' => $user->email, ':full_name' => $user->name, ':gender' => $user->gender, ':status' => $user->status]);
+    }
+
+    public function edit(User $user): bool{
+        $query = "UPDATE users SET name = {$user->name}, email = {$user->email} , gender = {$user->gender}, status = {$user->status} WHERE id = {$user->id}";
+        $statement = $this->connection->prepare($query);
+        return true;
     }
 }
--- app/Views/edit/editUser.php
+++ app/Views/edit/editUser.php
@@ -1,3 +1,8 @@
+<?php
+    $genders = ["male" => "Male", "female" => "Female"];
+    $statuses = ["active" => "Active", "inactive" => "Inactive"];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,26 +13,29 @@
 </head>
 <body>
     
-    <form action="/users" method="post">
+    <form action="/users/:userID" method="put">
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required value="<?=$user->email;?>">
 
         <label for="name">Your first and last name:</label>
-        <input type="text" id="name" name="name" required>
+        <input type="text" id="name" name="name" required value="<?=$user->name;?>">
 
         <label for="gender">Gender:</label>
         <select id="gender" name="gender" required>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
+            <?php foreach($genders as $value => $label):?>
+            <option value="<?=$value?>" <?= $value === $user->gender ? "selected" : "";?>><?=$label?></option>
+            <?php endforeach;?>
         </select>
 
         <label for="status">Status:</label>
-        <select id="status" name="status" required>
-            <option value="active">Active</option>
-            <option value="inactive">Inactive</option>
+        <select id="status" name="status" required">
+        <?php foreach($statuses as $value => $label):?>
+            <option value="<?=$value?>" <?= $value === $user->status ? "selected" : "";?>><?=$label?></option>
+            <?php endforeach;?>
         </select>
 
         <button type="submit" class="btn">Submit</button>
     </form>
 </body>
 </html>
+
