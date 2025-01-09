f4f4cc93251d929da68ab062b1da9834fc421c4f
EldarBruveris <eldar.bruverys@innowise.com>
2025-01-09 13:26:33 +0300

    ordering

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -18,7 +18,7 @@ class UserRepository extends AbstractRepository{
     //TODO выбрать всех из БД
     public function findAll(): array {
         $users = [];
-        $query = "SELECT * FROM users";
+        $query = "SELECT * FROM users ORDER BY id";
         $statement = $this->connection->query($query);
         while($row = $statement->fetch(PDO::FETCH_ASSOC)){
             $users[] = new User($row['id'], $row['email'], $row['full_name'], $row['gender'], $row['status']);
