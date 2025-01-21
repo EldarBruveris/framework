70e3668ae13ef5141da68c77ea2748c2fcfa6147
EldarBruveris <eldar.bruverys@innowise.com>
2025-01-21 07:14:44 +0300

    deleted unnecessary bootstrap folder

--- bootstrap/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-// spl_autoload_register(static function ($class) {
-//     $path = str_replace('\\', '/', $class) . '.php';
-
-//     require_once __DIR__ . '/../' . lcfirst($path);
-// });
--- public/index.php
+++ public/index.php
@@ -2,5 +2,4 @@
 
 require_once '../vendor/autoload.php';
 
-require_once __DIR__ . '/../bootstrap/autoload.php';
 require_once __DIR__ . '/../routes/route.php';
